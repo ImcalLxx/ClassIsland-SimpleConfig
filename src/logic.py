4117657fbda4cbc1946df39fd3e9b37848269ba4
ImcalLxx <imcalLszy@outlook.com>
2025-08-19 14:05:42 +0800

    feat:隔离设置模块到单独文件

--- src/logic.py
+++ src/logic.py
@@ -1,99 +1,14 @@
 # file: logic.py
 # brief: 逻辑处理模块
-# time: 2025.8.6
-# version: 0.1.0-Alpha-1
+# time: 2025.8.18
+# version: 0.1.0-Alpha-3
 # TODOs:
 #   暂无
 
-from PyQt5.QtCore import pyqtSignal, QThread, QObject
+from PyQt5.QtCore import pyqtSignal, QThread
 from eventbus     import EventBus
-from typing       import Any
 from loguru       import logger
-import time, orjson, json, os
-
-class Settings(QObject):
-    """
-    设置类
-    """
-
-    loadPriority: int = 0                                               # 加载优先级, 0=优先本程序, 1=优先ClassIsland
-    pathToCI: str = ""                                                  # ClassIsland可执行文件路径
-    eventBus: EventBus
-
-    def __init__(self, eventBus: EventBus, parent = None) -> None:
-        super().__init__(parent)
-        self.eventBus = eventBus
-
-    # 以下为事件处理的信号
-    # 需要与其他类通信的通过EventBus中继
-    ST_askForPathToCI_EH: pyqtSignal = pyqtSignal()
-
-    ST_returnPathToCI_LG: pyqtSignal = pyqtSignal(str)
-
-    def connectAllSingal(self) -> None:
-        """
-        连接所有信号
-        """
-
-        self.ST_askForPathToCI_EH.connect(self.eventBus.ST_askForPathToCI_EH)
-
-        def f1(self: Settings, pathToCI: str): self.pathToCI = pathToCI
-        self.eventBus.EH_returnPathToCI_ST.connect(lambda pathToCI: f1(self, pathToCI)) 
-
-        self.eventBus.EB_saveSettings_ST.connect(self.saveSettings)
-
-        self.ST_returnPathToCI_LG.connect(self.eventBus.ST_returnPathToCI_LG)
-        self.eventBus.LG_getPathToCI_ST.connect(lambda: self.ST_returnPathToCI_LG.emit(self.pathToCI))
-
-    def saveSettings(self) -> None:
-        """
-        保存设置选项
-        """
-
-        logger.info("开始保存设置文件到路径 './data/settings.json'")
-
-        data = {
-            "loadPriority": self.loadPriority,
-            "pathToCI": self.pathToCI
-        }
-
-        with open("./data/settings.json", "wb") as settingsFile:
-            settingsFile.write(orjson.dumps(data))
-
-        logger.success("保存设置文件成功")
-
-    def loadSettings(self) -> None:
-        """
-        加载设置选项
-        """
-
-        logger.info("开始从路径 './data/settings.json' 读取设置文件")
-
-        try:
-            with open("./data/settings.json", "r") as settingsFile:
-                data: dict[str, Any] = json.load(settingsFile)
-        except FileNotFoundError:
-            logger.error("读取设置文件时路径 './data/settings.json' 不存在")
-            return
-        
-        # TODO: 加入未找到键的报错处理
-        self.loadPriority = data["loadPriority"]
-        self.pathToCI = data["pathToCI"]
-
-    def init(self) -> None:
-        """
-        初始化, 检查设置文件完整性
-        """
-
-        if not os.path.exists("./data/settings.json"):
-            logger.warning("设置文件缺失, 重新创建")
-            self.saveSettings()
-        
-        self.loadSettings()
-
-        if self.pathToCI == "":
-            logger.warning("ClassIsland可执行文件路径为空, 现在询问用户")
-            self.ST_askForPathToCI_EH.emit()
+import time
 
 
 class Logic(QThread):
@@ -181,12 +96,11 @@ class Logic(QThread):
             if timeElasped < 0.0167:
                 time.sleep(0.0167 - timeElasped)                        # 计算休眠时间, 保持每秒60次刷新
 
-
     def run(self) -> None:
         """
         重写run方法
         """
 
-        time.sleep(1)                                                   # 睡1秒, 等GUI初始化 (才不是我懒得写事件循环和信号呢(
+        time.sleep(0.2)                                                   # 睡0.2秒, 等GUI初始化 (才不是我懒得写事件循环和信号呢(
         self.workMain()
         
